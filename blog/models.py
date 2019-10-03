fbedf0626f5b4ab5032ae56ef4363a05190ca454
yahsiPinar <pinarryahsi@gmail.com>
2019-10-03 13:08:52 +0300

    Django blog, first commit

---
+++ blog/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+
+#models.Model djangoya bu bir django modeli onu veritabanında saklaman gerek demek içindir.
+class Post(models.Model):
+    #ForeignKey başka bir modele referans tanımlar
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE) #postu yazan kişi
+    title = models.CharField(max_length=200) #postun başlığı
+    text = models.TextField() #içerik
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True,null=True)
+    def publish(self):
+        self.published_date=timezone.now()
+        self.save() #save() objeyi veritabanına kaydeder
+    def __str__(self):
+        return self.title
+
+# migration python modelinde yapılan değişikliklerle veritabanını bir araya getirmek için kullanılır.
+# makemigrations modelde yapılan değişikliklere göre yeni migrationlar oluşturur.
+# migrate bu migrationları veritabanına uygular.
+# modeli veritabanına eklemek için
+# python manage.py makemigrations blog
+# python manage.py migrate blog
