24fa2b2e614f063b4226f729f82c93fd75c0fbc6
yahsiPinar <pinarryahsi@gmail.com>
2019-10-03 22:52:11 +0300

    CSS dosyaları ve ayrıntı sayfaları eklendi.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views # bütün view ün dahil edilmesi
 
 urlpatterns = [
     path('',views.post_list, name = 'post_list'),
+    path('post/<int:pk>/', views.post_detail,name='post_detail'),
 ]
