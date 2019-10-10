11f4a5a69f07bd91c1f4f17218aaf5b8b5735c34
yahsiPinar <pinarryahsi@gmail.com>
2019-10-11 01:27:57 +0300

    Authorizing add/edit of posts

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from .models import Post
 from django.utils import timezone
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def post_list(request):
@@ -13,6 +14,7 @@ def post_detail(request,pk):
     post = get_object_or_404(Post,pk=pk)
     return render(request, 'blog/post_detail.html',{'post':post})
 
+@login_required()
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -26,6 +28,7 @@ def post_new(request):
         form = PostForm()
     return render(request,'blog/post_edit.html',{'form':form})
 
+@login_required()
 def post_edit(request,pk):
     post = get_object_or_404(Post,pk=pk)
     if request.method == "POST":
@@ -39,15 +42,18 @@ def post_edit(request,pk):
         form = PostForm(instance=post)
     return render(request,'blog/post_edit.html',{'form':form})
 
+@login_required()
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts':posts})
 
+@login_required()
 def post_publish(request,pk):
     post = get_object_or_404(Post,pk=pk)
     post.publish()
     return redirect('post_list')
-    
+
+@login_required()
 def post_remove(request,pk):
     post = get_object_or_404(Post,pk=pk)
     post.delete()
